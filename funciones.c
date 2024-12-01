61fe1ebb7967c3ad57b946ff3bf4c1f5b6a7d800
carruizdiaz1109 <cruiz@fi.uba.ar>
2024-12-01 20:19:44 -0300

    Se imprime la secuencia

--- funciones.c
+++ funciones.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 // Función para imprimir una cadena recibida desde ensamblador
-void imprimir_secuencia(char *secuencia) {
-    printf("Secuencia generada: %s\n", secuencia);
+void imprimir_secuencia(char* secuencia) {
+    printf("%s\n", secuencia); 
 }
